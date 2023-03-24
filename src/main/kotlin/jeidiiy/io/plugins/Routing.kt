ed1ffe0ba906690bc011689213f9fe61dd5c4dbd
Jeidiiy <jeidiiy98@gmail.com>
2023-03-24 23:00:53 +0900

    feat: Order API 개발

--- src/main/kotlin/jeidiiy/io/plugins/Routing.kt
+++ src/main/kotlin/jeidiiy/io/plugins/Routing.kt
@@ -3,9 +3,15 @@ package jeidiiy.io.plugins
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import jeidiiy.io.routes.customerRouting
+import jeidiiy.io.routes.getOrderRoute
+import jeidiiy.io.routes.listOrderRoute
+import jeidiiy.io.routes.totalizeOrderRoute
 
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrderRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
